767c23aa7242306114674564bb83694a80301b51
Takashiart2 <1603taka@gmail.com>
2021-12-08 10:16:49 +0000

    laravel10

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::group(['prefixx' =>'admin'], function() {
-    Route::get('news/create',
-'Admin\NeasController@add');
+Route::group(['prefix' =>'admin'], function() {
+     Route::get('news/create','Admin\NewsController@add');
+     Route::get('profile/create','ProfileController@add');
+     Route::get('profile/edit','ProfileController@edit');
 });
